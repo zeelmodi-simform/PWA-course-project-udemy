d68274472a2f8e01b3a448e88b0d56dc680bdd91
Zeel Modi <zeel.m@simformsolutions.com>
2023-10-16 15:44:22 +0530

    feat: Promise and Fetch

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,9 @@
 var deferredPrompt;
 
+if (!window.Promise) {
+    window.Promise = Promise;
+}
+
 navigator.serviceWorker.getRegistrations().then(function (registrations) {
     for (let registration of registrations) {
         console.log('Service Worker unregistered!');
@@ -12,6 +16,8 @@ if ('serviceWorker' in navigator) {
         // { scope: '/help/' }
     ).then(function () {
         console.log('Service Worker registered!')
+    }).catch((error) => {
+        console.log(error);
     });
 }
 
@@ -24,3 +30,64 @@ window.addEventListener('beforeinstallprompt', function (event) {
     return false
 
 })
+
+var promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve('This is executed once the timer is done')
+        reject({code: 500, message: 'An error occured!'})
+        // console.log('This is executed once the timer is done');
+    }, 3000);
+});
+
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://httpbin.org/ip');
+// xhr.responseType = 'json';
+// xhr.onload = function () {
+//     console.log(xhr.response);
+// }
+// xhr.onerror = function (error) {
+//     console.error(error);
+// }
+// xhr.send()
+
+fetch(`https://httpbin.org/ip`).then((response) => {
+    console.log(response);
+    return response.json()
+}).then((data) => {
+    console.log(data);
+}).catch((error) => {
+    console.error(error);
+})
+
+fetch(`https://httpbin.org/post`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        message: 'Does this work?'
+    }),
+    mode: 'cors'
+}).then((response) => {
+    console.log(response);
+    return response.json()
+}).then((data) => {
+    console.log(data);
+}).catch((error) => {
+    console.error(error);
+});
+
+promise.then((text) => {
+    return text
+},
+    // (err) => {
+    // console.log(error.code, error.message);  
+    // }
+).then((newText) => {
+    console.log(newText);
+}).catch((error) => {
+    console.log(error.code, error.message);
+});
+
+console.log('This is executed right after setTimeout()');
